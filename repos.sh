a332f18b8159d13944aeac07603b83973dac8a13
Dan Tovbein <dtovbein@gmail.com>
2022-03-16 14:04:55 -0300

    Force to add repos as a submodules

--- repos.sh
+++ repos.sh
@@ -6,12 +6,12 @@ git submodule update --init --recursive
 api="$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }' | grep src/api)"
 if [ -z "$api" ]
 then
-  git submodule add --name api https://github.com/GreenpeaceSkunk/greenlab-api.git src/api
+  git submodule add -f --name api https://github.com/GreenpeaceSkunk/greenlab-api.git src/api
 fi
 
 # Clone or Update Coupon
 coupon="$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }' | grep src/coupon)"
 if [ -z "$coupon" ]
 then
-  git submodule add --name coupon https://github.com/GreenpeaceSkunk/cupon-mercadopago.git src/coupon
+  git submodule add -f --name coupon https://github.com/GreenpeaceSkunk/cupon-mercadopago.git src/coupon
 fi
